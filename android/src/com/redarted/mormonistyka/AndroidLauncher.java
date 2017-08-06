b17f09de1e74e84287b7261f70ae875f9b9fc843
komp\Wojtek <w.mosienski@gmail.com>
2017-08-06 14:48:27 +0200

    initial commit

---
+++ android/src/com/redarted/mormonistyka/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.redarted.mormonistyka;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.redarted.mormonistyka.MormonistykaGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MormonistykaGame(), config);
+	}
+}
---
+++ html/src/com/redarted/mormonistyka/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.redarted.mormonistyka.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.redarted.mormonistyka.MormonistykaGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MormonistykaGame();
+        }
+}
